69eeb23c939f9579e74da1f9db220a429e07e3ac
Freddy Phoenix Mills <118795606+anonphoenix007@users.noreply.github.com>
2024-06-13 21:02:13 +0100

    Update patch.js

--- plugins/patch.js
+++ plugins/patch.js
@@ -369,7 +369,7 @@ ${menuThemeFooter}
         caption: menuText
       };
       //return await Void.sendMessage(message.chat, menuText);
-      return await message.sendUi(menuText)
+      return await message.bot.sendUi(menuText)
     } catch (error) {
       await message.error(error + "\nCommand: menu", error);
     }
