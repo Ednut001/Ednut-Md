82688f57c287e77f590a24cb61d40e231eee6154
Ednut <147618496+Ednut001@users.noreply.github.com>
2024-06-18 14:00:17 +0100

    Update _SySode.js

--- _SySode.js
+++ _SySode.js
@@ -45,7 +45,7 @@ smd({
 }, async (_0x2a10d6, _0x2420b0) => {
   try {
     let _0x4e5e2e = await plugins(_0x2a10d6, "plugins", _0x2420b0);
-    return await _0x2a10d6.send(!_0x4e5e2e ? "*_There's no plugin install in " + Config.botname + "_*" : !_0x2420b0 ? "*All Installed Modules are:-*\n\n" + _0x4e5e2e : _0x4e5e2e);
+    return await _0x2a10d6.send(!_0x4e5e2e ? "There's no plugin install in " + Config.botname + "." : !_0x2420b0 ? "All Installed Modules are:-\n\n" + _0x4e5e2e : _0x4e5e2e);
   } catch (_0x21e335) {
     _0x2a10d6.error(_0x21e335 + " \n\ncmdName plugins\n");
   }
@@ -60,7 +60,7 @@ smd({
   use: "<plugin name>"
 }, async (_0x1510c9, _0x40e763) => {
   if (!_0x40e763) {
-    return await _0x1510c9.reply("*_Uhh Please, Provide Me Plugin Name_*");
+    return await _0x1510c9.reply("hii kindly, Provide Me Plugin Name");
   }
   if (_0x40e763 === "alls") {
     return await _0x1510c9.reply(await plugins("remove", "all", __dirname));
@@ -79,7 +79,7 @@ smd({
 }, async (_0xf71b5c, _0x2bdd09) => {
   let _0x2b0828 = _0x2bdd09 ? _0x2bdd09 : _0xf71b5c.quoted ? _0xf71b5c.quoted.text : "";
   if (!_0x2b0828.toLowerCase().includes("https")) {
-    return await _0xf71b5c.send("*_Uhh Please, Provide Me Plugin Url_*");
+    return await _0xf71b5c.send("hii kindly, Provide Me Plugin Url");
   }
   await _0xf71b5c.reply(await plugins(_0xf71b5c, "install", _0x2b0828, __dirname));
 });
