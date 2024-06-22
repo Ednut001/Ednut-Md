78c04622cb0bf06747448707f506bf577b321361
Ednut <147618496+Ednut001@users.noreply.github.com>
2024-06-22 13:04:58 +0100

    Add files via upload

---
+++ plugins/ednut.js
@@ -0,0 +1,4 @@
+smd({ pattern: "ednut" }, async (Void, citel) => {
+
+  await Void.sendMessage(citel.chat, { text: "Hello it me ednut developer of ednut-md ✨ need any help contact me wa.me/2348102487241" }, { quoted: citel })
+})
